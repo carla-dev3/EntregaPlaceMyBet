78100e31fa32e2cebeb7885430a16e9d72727770
CarlaAparicio <carlaaparicio0905@gmail.com>
2020-09-15 16:32:47 +0200

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repaso1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Creación de listas
+            List<int> lista = new List<int>();
+            List<int> lista2 = new List<int>();
+
+            Añadir a la lista
+            lista.Add(2);
+            lista2.Add(2); */
+
+            // Para comparar usamos el CompareTo 
+
+
+            if (v1 == v2)
+            {
+                Console.WriteLine("Son iguales");
+            }
+            else
+            {
+                Console.WriteLine("Son distintas");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
